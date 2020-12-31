5fb12bbc95975bdba241fe3e77be2b2cfaedbf4b
Huafeng Wang <fvunicorn@gmail.com>
2020-12-31 19:52:23 +0800

    Update install-hologress-connector-to-local-mvn.sh

--- install-hologress-connector-to-local-mvn.sh
+++ install-hologress-connector-to-local-mvn.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn install:install-file -Dfile=library/hologres-flink-connector-1.10-jar-with-dependencies.jar -DgroupId=io.hologres -DartifactId=hologress-flink-connector -Dversion=1.10 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=hologres-flink-connector-1.10-jar-with-dependencies.jar -DgroupId=org.apache.flink -DartifactId=hologres-flink-connector -Dversion=1.10 -Dpackaging=jar -DgeneratePom=true
